63fd8b7224425d791ad1d7c0dd0ff7c7e5d826f1
Giorginel <giorgiocirjan22@gmail.com>
2026-05-07 11:20:21 +0200

    Actuator.h iniziato, non finito

---
+++ Actautor.h
@@ -0,0 +1,85 @@
+/*! \file Actuator.h
+    \brief Declaration of the class Actuator
+    \author Gruppo 3 : Racco Davide,Forciniti Nicolò,Bonelli Gianni,Cirjan Giorgio
+*/
+
+#ifndef ACTUATOR_H
+#define ACTUATOR_H
+
+#include"Room.h"
+
+//#include<ctime> 
+
+/// @class Actuator
+/// @brief to manage an Actuator 
+class Actuator 
+{
+protected:
+    
+public:
+
+    /// @name CONSTRUCTORS / DESTRUCTOR
+    /// @{
+        Actuator();
+        /// @brief Main Constructor that get an ID and the state of
+        /// @param deviceID unique, should be different for each actuator
+        /// @param status  
+        Actuator(int deviceID, bool status);
+
+        ~Actuator();
+
+    /// @}
+
+    /// @name OPERATORS
+    /// @{
+
+
+    /// @}
+
+    /// @name BASIC HANDLING
+    /// @{
+        ///@brief  
+        ///@return 
+        bool readState();
+    /// @}
+
+    /// @name GETTERS
+    /// @{
+
+
+        Actuator* GetActuator(); // TODO: Manage the pointer of the individual actuator object
+        ///@brief To get the current actuator state
+        ///@return the current actuator state 
+        bool GetState();
+    /// @}
+
+    /// @name SETTERS
+    /// @{
+        
+        /// @brief To set actuatorID
+        void SetID();
+
+
+    ///}@
+
+    /// @name DEBUG and SERIALIZATION
+    /// @{
+        ///@brief Dump all the actuators data stored 
+        void Dump();
+
+    /// @}
+    
+    
+    
+
+    private: 
+        ///@param deviceID unique Identifier.
+        int deviceID;
+
+        ///@param state 1 = is active, 0 = not active.
+        bool status;
+
+    
+};
+
+#endif
